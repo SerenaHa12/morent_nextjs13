bb9fdf1f66afc1becd3eab25ea236753d564a886
SerenaHa12 <serenavander.ha@gmail.com>
2023-06-24 01:07:04 +0700

    combobox

---
+++ morent/components/SearchManufacturer.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+
+import { Combobox, Transition } from '@headlessui/react';
+import { SearchManufacturerProps } from '@/types'
+import React from 'react'
+
+const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerProps) => {
+  return (
+    <div className='search-manufacturer'>
+      <Combobox>
+        <div className="realtive w-full"></div>
+      </Combobox>
+    </div>
+  )
+}
+
+export default SearchManufacturer
